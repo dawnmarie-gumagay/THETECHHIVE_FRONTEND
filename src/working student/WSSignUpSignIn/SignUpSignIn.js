08b351a985e5a4846f5cd362a28a174de968197d
Nyangsters <hoooootenygg@gmail.com>
2024-07-29 04:43:48 +0800

    Enhanced some of the elements and logics

--- src/working student/WSSignUpSignIn/SignUpSignIn.js
+++ src/working student/WSSignUpSignIn/SignUpSignIn.js
@@ -16,31 +16,33 @@ const SignUpSignIn = () => {
     navigate("/wslandingpage");
   }, [navigate]);
 
-  
-
   return (
     <div className="ws-sign-upsign-in">
       <img className="background" alt="" src="/bg1.png" />
 
       <div className="main-boxS" />
-      <div className="back-bgSS" onClick={onWSLANDINGPAGEClick} />
-      <img className="back-iconSS" alt="" src="/back.png" onClick={onWSLANDINGPAGEClick} />
+      <div className="back-button-container" onClick={onWSLANDINGPAGEClick}>
+        <div className="back-bgSS" />
+        <img className="back-iconSS" alt="Back" src="/back.png" />
+      </div>
 
       <img className="main-bg" alt="" src="/main-bg.png" />
       <img className="main-title" alt="" src="/TITLE.png" />
 
       <i className="welcome">WELCOME!</i>
 
-      <div className="signup-container" onClick={onSIGNUPClick}>
-        <div className="signup-box" />
-        <div className="signup-name">SIGN UP</div>
-      </div>
+      <div className="auth-buttons-container">
+        <div className="signup-container" onClick={onSIGNUPClick}>
+          <div className="signup-box" />
+          <div className="signup-name">SIGN UP</div>
+        </div>
 
-      <div className="line" />
-     
-      <div className="signin-container" onClick={onSIGNINClick}>
-        <div className="signin-box" />
-        <div className="signin-name">SIGN IN</div>
+        <div className="line" />
+      
+        <div className="signin-container" onClick={onSIGNINClick}>
+          <div className="signin-box" />
+          <div className="signin-name">SIGN IN</div>
+        </div>
       </div>
       
       <img className="wildcat-cutie" alt="" src="/wildcat-icon.png" />
