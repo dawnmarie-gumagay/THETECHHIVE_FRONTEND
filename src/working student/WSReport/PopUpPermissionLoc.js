c4d10027976cdd418b295772893f5eacfab1177c
carbonara <corbetea@gmail.com>
2024-08-22 21:12:58 +0800

    Edited PopUp Permission for location

--- src/working student/WSReport/PopUpPermissionLoc.js
+++ src/working student/WSReport/PopUpPermissionLoc.js
@@ -1,27 +1,56 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 import "./PopUpPermissionLoc.css";
 
-const PopUpPermissionLoc = () => {
+const PopUpPermissionLoc = (onClose) => {
   const navigate = useNavigate();
 
   const onGroupContainerClick = useCallback(() => {
     navigate("/pop-upreport");
   }, [navigate]);
 
+  const handleCancel = useCallback(() => {
+    console.log("Cancel button clicked"); // Add this line for debugging
+    onClose();
+  }, [onClose]);
+
   return (
-    <div className="pop-up-permission-loc1">
-      <div className="pop-up-permission-loc-item" />
-      <div className="tap-allow-to-let-the1">{`Tap Allow to let the application use Location Services `}</div>
-      <div className="rectangle-parent19" onClick={onGroupContainerClick}>
-        <div className="group-child41" />
-        <div className="cancel6">CANCEL</div>
+    <div className="PermissionPage2">
+      <div className="allow-location">
+      Tap Allow to let the application use Location Services
       </div>
-      <div className="rectangle-parent20" onClick={onGroupContainerClick}>
-        <div className="group-child41" />
-        <div className="cancel6">ALLOW</div>
-      </div>
-      <img className="zondiconslocation1" alt="" src="/zondiconslocation.svg" />
+
+      <Button
+        className="permission-cancel-button2"
+        variant="contained"
+        sx={{
+          borderRadius: "10px",
+          width: 115,
+          height: 40,
+          backgroundColor: "#8A252C",
+          "&:hover": { backgroundColor: "#A91D3A" }
+        }}
+        onClick={handleCancel} // Changed to handleCancel
+      >
+        CANCEL
+      </Button>
+
+      <Button
+        className="permission-allow-button2"
+        variant="contained"
+        sx={{
+          borderRadius: "10px",
+          width: 115,
+          height: 40,
+          backgroundColor: "#8A252C",
+          "&:hover": { backgroundColor: "#A91D3A" }
+        }}
+      >
+        ALLOW
+      </Button>
+
+      <img className="permission-location" alt="" src="/location2.png" />
     </div>
   );
 };
